46769960040614c62a38e9d1085bf239eb42200c
Beprados <beprados04@gmail.com>
2025-11-23 15:03:01 -0300

    Iniciando view 2 do Post-it

--- Postit.java
+++ Postit.java
@@ -1,7 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout; // Não é mais estritamente necessário, mas mantido
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 
